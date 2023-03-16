3d4b2adb77fa44df69a69fcb17b15eb2986e4796
welbert <welbert.jsj@gmail.com>
2023-03-16 00:37:53 -0300

    first commit

---
+++ src/main/java/com/br/motogear/productrelay/ProductRelayApplication.java
@@ -0,0 +1,13 @@
+package com.br.motogear.productrelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRelayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/motogear/productrelay/ProductRelayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.motogear.productrelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRelayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
